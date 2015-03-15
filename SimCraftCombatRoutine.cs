31e83e859b8846ad4e20e20b05171d8e42dfc875
agreian <lol@lol.lol>
2015-03-15 09:50:06 +0100

    Project creation

---
+++ SimCraftCombatRoutine.cs
@@ -0,0 +1,17 @@
+﻿using Styx.CommonBot.Routines;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimcBasedCoRo
+{
+    public class SimCraftCombatRoutine : CombatRoutine
+    {
+        public override string Name
+        {
+            get { return "SimcBasedCoRo"; }
+        }
+    }
+}
